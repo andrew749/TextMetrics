1471a52760f3edc522e685a9901f70c2313f5cc8
andrew749 <andrewcod749@gmail.com>
2014-04-06 00:42:51 -0400

    added conversations

---
+++ src/com/andrew749/textmetrics/Conversations.java
@@ -0,0 +1,16 @@
+package com.andrew749.textmetrics;
+
+public class Conversations {
+	public String address = "";
+	public int messages = 0;
+
+	public Conversations(String address, int number) {
+		this.address = address;
+		messages = number;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return address;
+	}
+}
